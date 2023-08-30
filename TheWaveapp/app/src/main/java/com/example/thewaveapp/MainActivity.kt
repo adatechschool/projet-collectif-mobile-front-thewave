832f9a3b85fbbf9375a844955f8759f6303304c5
SebFoodJ <sebastien.fougere@gmail.com>
2023-08-30 15:21:22 +0200

    Test Mise en page et Premiers textes

--- TheWaveapp/app/src/main/java/com/example/thewaveapp/MainActivity.kt
+++ TheWaveapp/app/src/main/java/com/example/thewaveapp/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.thewaveapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.thewaveapp.ui.theme.TheWaveAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
+
                 }
             }
         }
@@ -30,17 +38,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "Nom du Spot",
+            modifier = modifier.align(alignment = androidx.compose.ui.Alignment.CenterHorizontally)
+        )
+
+        Text(
+            text = "Description du spot de Surf",
+            modifier = modifier.align(alignment = androidx.compose.ui.Alignment.CenterHorizontally)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = null,
+            modifier = Modifier
+                .size(80.dp)
+                .fillMaxSize()
+                .padding(16.dp)
+        )// Create Button
+        Button(onClick = { /*TODO*/ }) {
+            var text = "Ajouter un spot"
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TheWaveAppTheme {
-        Greeting("Android")
+        Greeting()
+        SearchBar()
     }
 }
+
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "Rechercher un spot",
+            modifier = modifier.align(alignment = androidx.compose.ui.Alignment.CenterHorizontally)
+        )
+    }
+}
+
