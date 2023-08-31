4cbc77e1973aac10c2b1bd740627dd31d8788a6c
SebFoodJ <sebastien.fougere@gmail.com>
2023-08-31 14:12:44 +0200

    Test Mise en page et intégration image et textes

--- TheWaveapp/app/src/main/java/com/example/thewaveapp/MainActivity.kt
+++ TheWaveapp/app/src/main/java/com/example/thewaveapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,10 +14,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.thewaveapp.ui.theme.TheWaveAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,10 +31,9 @@ class MainActivity : ComponentActivity() {
             TheWaveAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting()
+                    Greeting(spotName = "Fonteau", spotPlace = "Spot de surf")
 
                 }
             }
@@ -38,47 +42,70 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
+fun Greeting(spotName: String, spotPlace: String, modifier: Modifier = Modifier) {
     Column {
         Text(
-            text = "Nom du Spot",
-            modifier = modifier.align(alignment = androidx.compose.ui.Alignment.CenterHorizontally)
+            text = spotName,
+            color = white,
+            fontSize = 80.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(6.dp)
         )
 
         Text(
-            text = "Description du spot de Surf",
-            modifier = modifier.align(alignment = androidx.compose.ui.Alignment.CenterHorizontally)
+            text = spotPlace,
+            fontSize = 36.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(16.dp)
         )
         Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
+            painter = painterResource(id = R.drawable.reefbreak),
             contentDescription = null,
             modifier = Modifier
-                .size(80.dp)
                 .fillMaxSize()
                 .padding(16.dp)
-        )// Create Button
-        Button(onClick = { /*TODO*/ }) {
-            var text = "Ajouter un spot"
+        )
         }
     }
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TheWaveAppTheme {
-        Greeting()
-        SearchBar()
+        GreetingImage(spotName = "Reef Break", spotPlace = "Pipeline, Oahu, Hawaii")
+        //SearchBar()
     }
 }
 
-@Composable
+/*@Composable
 fun SearchBar(modifier: Modifier = Modifier) {
     Column {
         Text(
             text = "Rechercher un spot",
             modifier = modifier.align(alignment = androidx.compose.ui.Alignment.CenterHorizontally)
         )
+    }
+}
+ */
+@Composable
+fun GreetingImage(spotName: String, spotPlace: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.fonteau2)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillHeight
+        )
+        Greeting(
+            spotName = spotName,
+            spotPlace = spotPlace,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+            )
     }
 }
 
