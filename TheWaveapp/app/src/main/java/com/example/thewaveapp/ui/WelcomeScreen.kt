48a14e41a9c4eae6e79de4240d48c69c4b5ff6ce
SebFoodJ <sebastien.fougere@gmail.com>
2023-09-04 14:43:17 +0200

    Test

--- TheWaveapp/app/src/main/java/com/example/thewaveapp/ui/WelcomeScreen.kt
+++ TheWaveapp/app/src/main/java/com/example/thewaveapp/ui/WelcomeScreen.kt
@@ -1,15 +1,31 @@
 package com.example.thewaveapp.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.thewaveapp.R
 
-@compose
-fun WelcomeScreen() {
+@Composable
+fun WelcomeScreen(
+    modifier: Modifier = Modifier
+    onClick: () -> Unit
+) {
     Column {
-        Image(painter = , contentDescription = )
+        Image(painter = painterResource(R.drawable.wavelogo1) , contentDescription = "logo")
         Text(text = "The Wave")
 
     Button(onClick = { /*TODO*/ }) {
             Text(text = "Welcome to The Wave")
     }
+}
+    
+@Preview
+@Composable
+fun WelcomeScreenPreview() {
+    WelcomeScreen()
 }
